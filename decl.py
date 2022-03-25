695a0489fdea32978bf0d7304b9abcdbdd485b5b
wediaklup <101984195+wediaklup@users.noreply.github.com>
2022-03-25 19:49:51 +0100

    Add files via upload

---
+++ decl.py
@@ -0,0 +1,101 @@
+import logging
+
+# logging.basicConfig(filename='example.log', encoding='utf-8', level=logging.DEBUG)
+
+
+dictionary = {}
+
+
+def dekl(mode, inflekativ, arg1="0", arg2="0", arg3="0"):
+    if mode == "subst":
+        # arg1 = Kasus
+        # arg2 = Numerus
+        if arg1 == "0" and arg2 == "0":
+            result = Kasus.sg.dekl(inflekativ)
+            tmp = Kasus.pl.dekl(inflekativ)
+            for i in range(len(tmp)):
+                result.append(tmp[i])
+            return result
+        elif arg1 == "0" or arg2 == "0":
+            logging.warning("Deklinieren: Unzureichende Parameter")
+        else:
+            pass
+
+
+def fullformat(mode, liste):
+    if mode == "subst":
+        resultsg = f"Singular\nNominativ: {liste[0]}\nAkkusativ: {liste[1]}\nLokativ: {liste[2]}\nOrginativ: {liste[3]}\nDirektativ: {liste[4]}\nInstrumentativ: {liste[5]}\nPossessiv: {liste[6]}\nTotalitiv: {liste[7]}\n\n"
+        resultpl = f"Plural\nNominativ: {liste[8]}\nAkkusativ: {liste[9]}\nLokativ: {liste[10]}\nOrginativ: {liste[11]}\nDirektativ: {liste[12]}\nInstrumentativ: {liste[13]}\nPossessiv: {liste[14]}\nTotalitiv: {liste[15]}"
+        return (resultsg + resultpl)
+
+
+class Kasus:
+    class sg:
+        nom = "энзу"
+        akk = "энио"
+        lok = "эногōрт"
+        org = "эногōиаλ"
+        dir = "эногōиоλ"
+        ins = "эногōба"
+        pos = "эногōрy"
+        tot = "эногōга"
+
+        @staticmethod
+        def dekl(wort):
+            res = [wort + Kasus.sg.nom,
+                   wort + Kasus.sg.akk,
+                   wort + Kasus.sg.lok,
+                   wort + Kasus.sg.org,
+                   wort + Kasus.sg.dir,
+                   wort + Kasus.sg.ins,
+                   wort + Kasus.sg.pos,
+                   wort + Kasus.sg.tot]
+            return res
+
+    class pl:
+        nom = "казу"
+        akk = "каио"
+        lok = "каогōрт"
+        org = "каогōиаλ"
+        dir = "каогōиоλ"
+        ins = "каогōба"
+        pos = "каогōрy"
+        tot = "каогōга"
+
+        @staticmethod
+        def dekl(wort):
+            res = [wort + Kasus.pl.nom,
+                   wort + Kasus.pl.akk,
+                   wort + Kasus.pl.lok,
+                   wort + Kasus.pl.org,
+                   wort + Kasus.pl.dir,
+                   wort + Kasus.pl.ins,
+                   wort + Kasus.pl.pos,
+                   wort + Kasus.pl.tot]
+            return res
+
+
+class Wort:
+    def __init__(self, wort, wortart):
+        pass
+
+
+class Substantiv:
+    def __init__(self, inflekativ, wortart, ipa, definition, deklination):
+        pass
+
+
+def load():
+    """with open("liste.kaz", "r") as f:
+        for lines in f:
+            pass"""
+    f = open("liste.kaz", "r")
+    data = f.read()
+    f.close()
+    data = data.split("\n")
+    return data
+
+if __name__ == '__main__':
+    while True:
+        wrt = input("Zu deklinieren> ")
+        print(fullformat("subst", dekl("subst", wrt)) + "\n")
---
+++ dict-engine.py
@@ -0,0 +1,35 @@
+import decl
+
+wörter = {}
+
+class Wort:
+    def __init__(self, kaz: str, ipa: str, deu: list, wortart: str, definition: str, tags: list, inflekativ: str):
+        self.kaz = kaz
+        self.deu = deu
+        self.ipa = ipa
+        self.wortart = wortart
+        self.definition = definition
+        self.tags = tags
+        self.inflekativ = inflekativ
+
+def load():
+    tmp_return = {}
+    with open("liste.kaz", "r") as f:
+        data = f.read().split("\n")
+        print(f"Inital\t{data}")
+        for word in data:
+            tmp_return[getattr(data[word], 'kaz')] = data[word]
+        print(tmp_return)
+            
+
+# Init
+
+#wörter = load()
+load()
+
+#Wort('èзyнэнзy', '[ɛzuːnənzu]', 'subst', ['Monster'], 'Räumlichkeit, in der ein Handelsunternehmen, ein gewerbliches Unternehmen Waren ausstelt und zum Verkauf anbietet', ['P1'], 'èзyн')
+#Wort('èзyнÿтно', '[ɛzuːnytnɔ]', 'verb', ['kaufen'], 'etwas gegen Bezahlung erwerben', ['Q1'], 'èзyн')
+#Wort('èзyнλи', '[ɛzuːnli]', 'adj', ['käuflich', 'bestechlich'], '[1] gegen Geld erhältlich;\n[2] bestechlich', ['R1'], 'èзyн')
+#Wort('монэнзy', '[ˈmɔnˌənzu]', 'subst', ['Monster'], 'furchterregendes, hässliches Fabelwesen, Ungeheuer von fantastischer, meist riesenhafter Gestalt', ['P2'], 'мон')
+#Wort('монÿтно', '[ˈmɔnˌytnɔ]', 'verb', ['etwas fürchten'], '[1] vor jemandem, etwas Angst haben;\n[2] Unangenehmes ahnen, befürchten;\n[3] Furcht empfinden, Angst haben', ['Q2'], 'мон')
+#Wort('монλи', '[ˈmɔnˌli]', 'adj', ['gruselig'], 'sich vor etwas Unheimlichem, Makabrem o. Ä. fürchten; Grausen, Furcht empfinden', ['R3'], 'мон')
