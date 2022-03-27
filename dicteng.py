2c3f35c410684ef2d546b96dc1fa7ae8b9ea2c2a
wediaklup <101984195+wediaklup@users.noreply.github.com>
2022-03-27 15:33:06 +0200

    First pre-release
    Added support for Adjectives, Pronouns, Conjunctions and Prepositions; new words; debug mode

--- dicteng.py
+++ dicteng.py
@@ -1,6 +1,8 @@
 import decl
 import os
 
+DEBUG = False
+
 
 class Wort:
     def __init__(self, kaz: str, stransc: str, wortart: str, deu: list, definition: str, tags: list, inflekativ: str):
@@ -22,12 +24,23 @@ class Wort:
             return 'Verb'
         elif self.wortart == 'adj':
             return 'Adjektiv'
+        elif self.wortart == 'pron':
+            return 'Pronomen'
+        elif self.wortart == 'conj':
+            return 'Konjunktion'
+        elif self.wortart == 'adv':
+            return 'Adverb'
+        elif self.wortart == 'prepo':
+            return 'Präposition'
 
     def fullformat(self):
         trans = str(self.deu)
         trans = trans.strip("[")
         trans = trans.strip("]")
-        return f"{self.kaz}\n{self.stransc}\n{self.wortart_format()}\nvon {self.inflekativ}\n\n{trans}\n{self.definition}"
+        if self.wortart == 'subst' or self.wortart == 'verb' or self.wortart == 'adj' or self.wortart == 'pron':
+            return f"{self.kaz}\n{self.stransc}\n{self.wortart_format()}\nvon {self.inflekativ}\n\n{trans}\n{self.definition}"
+        else:
+            return f"{self.kaz}\n{self.stransc}\n{self.wortart_format()}\n\n{trans}\n{self.definition}"
 
 
 def load():
@@ -59,32 +72,41 @@ def save():
 
 worter = load()
 print(worter)
-            
+
+with open("debug.ini", "r") as dbg:
+    try:
+        DEBUG = dbg.read()
+    except Exception as e:
+        with open("errors.log", "w") as er:
+            er.write(f"Couldn't read debug.ini: {e}")
+
 
 def add():
+    os.system("cls")
     a_tags = []
     a_deu = []
     print("# Add or modify #")
     print("Daten ausfüllen")
-    a_kaz = input("Name (Kaseiisch)>\t")
+    a_kaz = input("Name (Kaseiisch)>\t\t\t\t\t")
     a_deuraw = input("Deutsche Übersetzungen (mit 'BREAK' fortfahren)>\t")
     a_deu.append(a_deuraw)
     while a_deuraw != 'BREAK':
         a_deuraw = input("Deutsche Übersetzungen (mit 'BREAK' fortfahren)>\t")
         if a_deuraw != 'BREAK':
             a_deu.append(a_deuraw)
-    a_stransc = input("Wiss. Transkription>\t")
-    a_wortart = input("Wortart>\t")
-    a_definition = input("Definition(en)>\t")
-    a_tagsraw = input("Tags (mit 'BREAK' fortfahren)>\t")
+    a_stransc = input("Wiss. Transkription>\t\t\t\t\t")
+    a_wortart = input("Wortart>\t\t\t\t\t\t")
+    a_definition = input("Definition(en)>\t\t\t\t\t\t")
+    a_tagsraw = input("Tags (mit 'BREAK' fortfahren)>\t\t\t\t")
     a_tags.append(a_tagsraw)
     while a_tagsraw != 'BREAK':
-        a_tagsraw = input("Tags (mit 'BREAK' fortfahren)>\t")
+        a_tagsraw = input("Tags (mit 'BREAK' fortfahren)>\t\t\t\t")
         if a_tagsraw != 'BREAK':
             a_tags.append(a_tagsraw)
-    a_infl = input("Inflekativ>\t")
+    a_infl = input("Inflekativ>\t\t\t\t\t\t")
     os.system("cls")
-    print(f"= Ist das korrekt? =\n{a_kaz}\n{a_stransc}\n{a_wortart}; {a_tags}\n\n{a_deu}\n{a_definition}\n{a_infl}\n\n{worter}\n\n")
+    print(
+        f"= Ist das korrekt? =\n{a_kaz}\n{a_stransc}\n{a_wortart}; {a_tags}\n\n{a_deu}\n{a_definition}\n{a_infl}\n\n{worter}\n\n")
     x = input("Y/n> ")
     if x.upper() == 'Y':
         worter[a_kaz] = Wort(a_kaz, a_stransc, a_wortart, a_deu, a_definition, a_tags, a_infl)
@@ -212,34 +234,82 @@ def search(inp):
         e_genusverbi = 'Passiv'
         e_tempus = 'Zukunft'
         inflekativ = inp[:-5]
+    # Adjektive
+    elif inp[-2:] == 'λи':
+        e_wortart = 'adj'
+        e_fall = 'Infinitiv'
+        inflekativ = inp[:-2]
+    elif inp[-2:] == 'зă':
+        e_wortart = 'adj'
+        e_fall = 'Nominativ'
+        inflekativ = inp[:-2]
+    elif inp[-2:] == 'их':
+        e_wortart = 'adj'
+        e_fall = 'Akkusativ'
+        inflekativ = inp[:-2]
+    elif inp[-4:] == 'ōера':
+        e_wortart = 'adj'
+        e_fall = 'Lokativ'
+        inflekativ = inp[:-4]
+    elif inp[-5:] == 'ōеаиа':
+        e_wortart = 'adj'
+        e_fall = 'Orginativ'
+        inflekativ = inp[:-5]
+    elif inp[-5:] == 'ōеōиō':
+        e_wortart = 'adj'
+        e_fall = 'Direktativ'
+        inflekativ = inp[:-5]
+    elif inp[-4:] == 'ōебö':
+        e_wortart = 'adj'
+        e_fall = 'Instrumentativ'
+        inflekativ = inp[:-4]
+    else:
+        inflekativ = 'N/A'
 
     form = ""
     if e_wortart == 'subst':
         form = f"{e_fall} {e_numerus}"
     elif e_wortart == 'verb':
         form = f"{e_tempus} {e_genusverbi}"
+    elif e_wortart == 'adj':
+        form = f"{e_fall}"
 
     infls = []
     fin = ""
     for wr in worter:
-        if worter[wr].inflekativ == inflekativ:
-            infls.append(wr)
+        if worter[wr].wortart == e_wortart or 'pron' or 'conj' or 'adv':
+            if worter[wr].inflekativ == inflekativ:
+                infls.append(wr)
+        else:
+            if wr == inp:
+                infls.append(wr)
+
+    print(infls)
 
     for il in infls:
-        if worter[il].wortart == e_wortart:
+        if worter[il].wortart == e_wortart or worter[il].wortart == 'pron':
+            fin = worter[il]
+            print(f"{worter[il].wortart} == {e_wortart} oder 'pron'")
+        elif il == inp:
+            print(f"{il} == {inp}")
             fin = worter[il]
 
-    print(f"{inp} ist {form} von:\n")
+    print(fin)
+
+    if DEBUG == 'False':
+        os.system("cls")
+
+    if e_wortart != '':
+        print(f"{inp} ist {form} von:\n")
+
     return fin.fullformat()
 
 
 # Init
 
-#wörter = load()
+# wörter = load()
 
 if __name__ == '__main__':
     os.system("cls")
     while True:
         add()
-
-
--- dictionary.py
+++ dictionary.py
@@ -5,6 +5,10 @@ while True:
     os.system("cls")
     print("== Kaseiisch Wörterbuch ==\nZu suchendes Wort eingeben!")
     term = input("> ")
-    os.system("cls")
-    print(dicteng.search(term))
+    try:
+        print(dicteng.search(term))
+    except Exception as e:
+        if dicteng.DEBUG == 'False':
+            os.system("cls")
+        print("Wort nicht gefunden\n{e}".format(e=e))
     input()
